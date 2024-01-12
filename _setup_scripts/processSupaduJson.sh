42fa7ba56a76b0910a88e2430542a52b2197e699
Laura Henze <lh435@nyu.edu>
2024-01-12 11:46:25 -0500

    yearBook and dateBook
    dateBook not used currently

--- _setup_scripts/processSupaduJson.sh
+++ _setup_scripts/processSupaduJson.sh
@@ -10,13 +10,16 @@ else
 	exit
 fi
 
-for filename in $(find $1 -type f -name "intake-descriptive.json"); do 
+for filename in $(find $1 -type f -name "intake-descriptive.json"); do
   testtitle=$(jq -r  '.title' $filename)
   isbn=$(jq -r  '.identifier' $filename)
-  echo Processing: $testtitle: $filename 	
+  echo Processing: $testtitle: $filename
   # split is a jq function, not bash
   # https://jqplay.org/s/lKZtOV9VR4L
-  jq --sort-keys '.["subjects"] = (.subject | sub ("Latino\\\\a Studies"; "latino-a-studies") | split(" / "))| .["isbn"] = .identifier | .["series"] = .series_names | .["bookpubdate"] = .date | .["date"] = .date + "-01-01" | .["citation_link"] = .permanent_url | del(.type, .subject, .identifier, .permanent_url) ' $filename > "../content/books/$isbn.md"
+  # isbn is now openSquareId
+  # date is now dateBook in the JSON so will not need to be transformed
+  # yearBook is new field
+  jq --sort-keys '.["subjects"] = (.subject | sub ("Latino\\\\a Studies"; "latino-a-studies") | split(" / "))| .["isbn"] = .identifier | .["series"] = .series_names | .["date"] = .date + "-01-01" | .["citation_link"] = .permanent_url | del(.type, .subject, .identifier, .permanent_url) ' $filename > "../content/books/$isbn.md"
   echo done
   echo ...
 done
